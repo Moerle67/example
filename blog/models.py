c8bc1e09aca7076a26334a93eb4274c2d9c4c02b
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2023-07-22 13:35:24 +0200

    Eigener Objektmanager

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status = Post.Status.PUBLISHED)
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -19,6 +22,9 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, 
                               default=Status.DRAFT)
+    
+    objects = models.Manager()     # Default Manager
+    published = PublishedManager() # Eigener Manager
     class Meta:
         ordering = ['-publish']
         indexes = [
